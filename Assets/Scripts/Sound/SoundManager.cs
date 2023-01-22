b8c2b78c393d578755989addc9d78e5825dc1a0c
Juliette Sabatier <juliette.sabatier07@gmail.com>
2023-01-22 13:31:34 +0100

    sound manager and sound when player tangible is placed

---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class SoundManager: MonoBehaviour
+    {
+        public static SoundManager Instance;
+
+        [SerializeField]
+        private AudioSource _musicSource, _effectsSource;
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public void PlaySound(AudioClip clip)
+        {
+            _effectsSource.PlayOneShot(clip);
+        }
+    }
+}
